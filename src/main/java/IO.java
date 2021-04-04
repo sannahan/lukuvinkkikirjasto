91a0cedb4a8fcdeb13dbb2ae7fd1960b1b98f0f3
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-04 13:06:34 +0300

    tektikäyttöliittymä ja io-rajapinta alustava rakenne

--- src/main/java/IO.java
+++ src/main/java/IO.java
@@ -1,5 +1,5 @@
 public interface IO {
-    public void print(String msg);
-    public String nextLine(String msg);
-    public void error(String msg);
+    public void komento();
+    public void lisaaVinkki();
+    public void selaaVinkkeja();
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         IO textUI = new TextUI(new Scanner(System.in));
-
-        textUI.print("Hello Lukuvinkkikirjasto!");
-
-        String komento = textUI.nextLine("Komento :");
-        textUI.error("Komentoa \"" + komento + "\" ei löydy!");
+        textUI.komento();
     }
 }
 
